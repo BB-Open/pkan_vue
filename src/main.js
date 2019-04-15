b3f48b9ea7556c16c53adc019eb8a84ffa318290
sandra <sandra.rum@inqbus.de>
2019-04-15 17:38:05 +0200

    First Running example with websocket

--- src/main.js
+++ src/main.js
@@ -2,12 +2,37 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import {WEBSOCKET_URL} from './config';
+// static (non reactive data)
+import VueStatic from 'vue-static'
+// Logging related
+import VueLogger from 'vuejs-logger';
+// utilizing websockets
+import VueSocketIO from 'vue-socket.io';
+// use the namespaced variant which states clearer which properties are non reactive
+Vue.use(VueStatic, {namespaced: true});
+const isProduction = process.env.NODE_ENV === 'production';
+const options = {
+  isEnabled: true,
+  logLevel: isProduction ? 'error' : 'debug',
+  stringifyArguments: false,
+  showLogLevel: true,
+  showMethodName: true,
+  separator: '|',
+  showConsoleColors: true
+};
+Vue.use(VueLogger, options);
+// socketIO connection
+Vue.use(new VueSocketIO({
+  debug: true,
+  connection: WEBSOCKET_URL,
+}));
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   components: { App },
   template: '<App/>'
-})
+});
