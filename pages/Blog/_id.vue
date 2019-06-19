0fe74de2d3d205a1ac50422faa71cb2f43648a95
sandra <sandra.rum@inqbus.de>
2019-06-19 13:01:02 +0200

    Removed authentication

--- pages/Blog/_id.vue
+++ pages/Blog/_id.vue
@@ -24,7 +24,6 @@
       return {
         namespace: 'Blog ID',
         result: {},
-        token_res: null,
         item: {
           title: '',
           description: '',
@@ -32,8 +31,7 @@
             data: ''
           }
         },
-        data_url: PLONE_URL + '/Plone/@entity?fullobjects=1&UID=',
-        login_url: PLONE_URL + '/Plone/@login'
+        data_url: PLONE_URL + '/Plone/@search?fullobjects=1&UID=',
       }
     },
     mounted() {
@@ -45,42 +43,21 @@
     methods: {
       get_data() {
         // todo: this is not nice
-        let $this = this;
         let uid = this.$route.params.id;
-        if (this.token_res === null) {
-          this.login().then(function (res) {
-            $this.token_res = res;
-            $this.$log.debug(res);
-            $this.request_pages($this.data_url + uid)
-          });
-        } else {
-          this.request_pages(this.data_url + uid)
-        }
+
+        this.request_pages(this.data_url + uid)
+
       },
       async request_pages(url) {
 
         this.$axios.setHeader('Content-Type', 'application/json', ['get']);
         this.$axios.setHeader('Accept', 'application/json', ['get']);
         this.$axios.setHeader('Access-Control-Allow-Origin', '*', ['get']);
-        this.$axios.setHeader('Authorization', 'Bearer ' + this.token_res);
         this.result = await this.$axios.$get(url);
         this.item = this.result.items[0];
         this.$log.debug(this.item);
         this.$forceUpdate()
       },
-
-
-      async login() {
-        let data = {
-          login: "admin",
-          password: "admin"
-        };
-        this.$axios.setHeader('Content-Type', 'application/json', ['post']);
-        this.$axios.setHeader('Accept', 'application/json', ['post']);
-        this.$axios.setHeader('Access-Control-Allow-Origin', '*', ['post']);
-        let res = await this.$axios.$post(this.login_url, data);
-        return res['token']
-      }
     }
 
   }
--- pages/Blog/index.vue
+++ pages/Blog/index.vue
@@ -28,9 +28,7 @@
       return {
         namespace: 'Blog',
         result: {},
-        token_res: null,
-        data_url: PLONE_URL + '/Plone/@entity?portal_type=Document&fullobjects=1',
-        login_url: PLONE_URL + '/Plone/@login'
+        data_url: PLONE_URL + '/Plone/@search?portal_type=Document&fullobjects=1',
       }
     },
     mounted() {
@@ -41,40 +39,16 @@
     },
     methods: {
       get_data() {
-        // todo: this is not nice
-        let $this = this;
-        if (this.token_res === null) {
-          this.login().then(function (res) {
-            $this.token_res = res;
-            $this.$log.debug(res);
-            $this.request_pages($this.data_url)
-          });
-        } else {
-          this.request_pages(this.data_url)
-        }
+        this.request_pages(this.data_url)
       },
       async request_pages(url) {
 
         this.$axios.setHeader('Content-Type', 'application/json', ['get']);
         this.$axios.setHeader('Accept', 'application/json', ['get']);
         this.$axios.setHeader('Access-Control-Allow-Origin', '*', ['get']);
-        this.$axios.setHeader('Authorization', 'Bearer ' + this.token_res);
         this.result = await this.$axios.$get(url);
         this.$forceUpdate()
       },
-
-
-      async login() {
-        let data = {
-          login: "admin",
-          password: "admin"
-        };
-        this.$axios.setHeader('Content-Type', 'application/json', ['post']);
-        this.$axios.setHeader('Accept', 'application/json', ['post']);
-        this.$axios.setHeader('Access-Control-Allow-Origin', '*', ['post']);
-        let res = await this.$axios.$post(this.login_url, data);
-        return res['token']
-      }
     }
 
   }
