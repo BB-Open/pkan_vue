d6c32cd0180ffaa8d507b36a319cd3c21b2c7687
sandra <sandra.rum@inqbus.de>
2019-12-03 16:22:11 +0100

    New Server Settings

--- components/configs/server_settings.js
+++ components/configs/server_settings.js
@@ -1,4 +1,4 @@
 export const WEBSOCKET_URL = 'https://flask.datenadler.de';
-export const PLONE_URL = 'https://new.datenadler.de/Plone';
+export const PLONE_URL = 'https://backend.datenadler.de';
 export const MY_URL = 'https://frontend.datenadler.de';
 export const RDF_DOWNLOAD_URL = WEBSOCKET_URL + '/download';
