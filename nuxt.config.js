ff0b7544433efafe7c493a202d7bf68584d9b3fa
Thomas Massmann <thomas.massmann@it-spir.it>
2019-12-06 18:11:40 +0100

    Make IE11 work.

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,83 +1,90 @@
-import pkg from './package'
+import pkg from "./package";
 
 const logOptions = {
   // optional : defaults to true if not specified
   isEnabled: true,
   // required ['debug', 'info', 'warn', 'error', 'fatal']
-  logLevel : 'debug',
+  logLevel: "debug",
   // optional : defaults to false if not specified
-  stringifyArguments : false,
+  stringifyArguments: false,
   // optional : defaults to false if not specified
-  showLogLevel : false,
+  showLogLevel: false,
   // optional : defaults to false if not specified
-  showMethodName : false,
+  showMethodName: false,
   // optional : defaults to '|' if not specified
-  separator: '|',
+  separator: "|",
   // optional : defaults to false if not specified
   showConsoleColors: false
 };
 
 export default {
-  mode: 'spa',
+  mode: "spa",
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
-    title: 'Datenadler.de',
+    title: "Datenadler.de",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: pkg.description }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    script: [{ src: "/js/object-assign.js" }]
   },
 
   /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
+   ** Customize the progress-bar color
+   */
+  loading: { color: "#fff" },
 
   /*
-  ** Global CSS
-  */
+   ** Global CSS
+   */
   css: [
-    'assets/styles.css',
-    'assets/plone_styles.css',
-    'assets/font_awesome/font-awesome.min.css'
+    "assets/styles.css",
+    "assets/plone_styles.css",
+    "assets/font_awesome/font-awesome.min.css"
   ],
 
   /*
-  ** Plugins to load before mounting the App
-  */
+   ** Plugins to load before mounting the App
+   */
   plugins: [
-    './plugins/vue_static',
-    './plugins/vue_socket_io',
-    './plugins/event_bus',
-    './plugins/infinite_scroll',
-    './plugins/VueMQ',
+    "./plugins/vue_static",
+    "./plugins/vue_socket_io",
+    "./plugins/event_bus",
+    "./plugins/infinite_scroll",
+    "./plugins/VueMQ"
   ],
 
   /*
-  ** Nuxt.js modules
-  */
-  modules: [
-    ['nuxt-log', logOptions],
-    '@nuxtjs/axios',
-  ],
+   ** Nuxt.js modules
+   */
+  modules: [["nuxt-log", logOptions], "@nuxtjs/axios"],
   axios: {
     // proxyHeaders: false
   },
 
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** You can extend webpack config here
-    */
-    extend(config, ctx) {
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {},
+    transpile: ["vue-socket.io"],
+    babel: {
+      presets: [
+        [
+          "@babel/env",
+          {
+            // useBuiltIns: true
+            targets: { ie: 10, uglify: true }
+          }
+        ]
+      ]
     }
   }
-}
+};
