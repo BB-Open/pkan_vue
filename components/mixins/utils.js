20124d597224006aeb5798db272eb913e7568a39
sandra <sandra.rum@inqbus.de>
2020-04-28 10:06:53 +0200

    Removed datepicker, try except for document in utils

--- components/mixins/utils.js
+++ components/mixins/utils.js
@@ -1,4 +1,5 @@
 import {format, startOfDay,} from 'date-fns'
+import Vue from 'vue';
 
 export const DATE_FORMAT = 'YYYY-MM-DD';
 
@@ -71,15 +72,24 @@ export function set_error_message(obj, message) {
   obj.$store.ep_commit('GlobalState', 'error_message', message);
 }
 
-const pkan_aria_plolite = document.getElementById('pkan_aria_polite');
-const pkan_aria_assertive = document.getElementById('pkan_aria_assertive');
+if (Vue.prototype.$isServer) {
+} else {
+  const pkan_aria_plolite = document.getElementById('pkan_aria_polite');
+  const pkan_aria_assertive = document.getElementById('pkan_aria_assertive');
+}
 
 export function write_aria_polite(message) {
-  pkan_aria_assertive.innerHTML = '';
-  pkan_aria_plolite.innerHTML = message
+  if (Vue.prototype.$isServer) {
+  } else {
+    pkan_aria_assertive.innerHTML = '';
+    pkan_aria_plolite.innerHTML = message
+  }
 }
 
 export function write_aria_assertive(message) {
-  pkan_aria_plolite.innerHTML = '';
-  pkan_aria_assertive.innerHTML = message
+  if (Vue.prototype.$isServer) {
+  } else {
+    pkan_aria_plolite.innerHTML = '';
+    pkan_aria_assertive.innerHTML = message
+  }
 }
--- plugins/datepicker.js
+++
@@ -1,4 +0,0 @@
-import Vue from 'vue';
-import Datepicker from "vuejs-datepicker";
-
-Vue.component('Datepicker', Datepicker);
