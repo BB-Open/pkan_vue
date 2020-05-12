fcdbb6eddeb5ab047d58abc05680a258f823561e
volker <volker.jaenisch@inqbus.de>
2020-05-12 13:45:24 +0200

    Fix: Added missing this to VUEX-STORE. But still no working aria-active.

--- components/controls/aria_active.vue
+++ components/controls/aria_active.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
     <div class="hidden_help_text" role="status" aria-live="polite" aria-relevant="all" aria-atomic="true" id="pkan_aria_polite">
-      {{aria_polite}} huhu
+      {{aria_polite}}
     </div>
     <div class="hidden_help_text" role="status" aria-live="assertive" aria-relevant="all" aria-atomic="true" id="pkan_aria_assertive">
-      {{aria_assertive}} huhu
+      {{aria_assertive}}
     </div>
   </div>
 </template>
@@ -20,12 +20,12 @@
     computed:{
       aria_polite: {
         get : function () {
-          this.$store.ep_get(VUEX_NAMESPACE, 'aria_polite')
+          this.$store.ep_get(this.VUEX_NAMESPACE, 'aria_polite')
         }
       },
       aria_assertive: {
         get : function () {
-          this.$store.ep_get(VUEX_NAMESPACE, 'aria_assertive')
+          this.$store.ep_get(this.VUEX_NAMESPACE, 'aria_assertive')
         }
       },
     }
