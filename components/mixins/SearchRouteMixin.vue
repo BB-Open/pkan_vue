35b3bc921b4a7546bddf0f2ce7bf8a2723b36fca
sandra <sandra.rum@inqbus.de>
2021-11-03 10:54:37 +0100

    BugFix: Missing Namespace

--- components/mixins/SearchRouteMixin.vue
+++ components/mixins/SearchRouteMixin.vue
@@ -3,20 +3,24 @@
 </template>
 
 <script>
+  import { VUEX_NAMESPACE } from '../../store/search_detail'
+
   export default {
     name: 'SearchRouteMixin',
     async beforeRouteLeave (to, from, next) {
+      let ns = VUEX_NAMESPACE
       this.$log.info('beforeRouteLeave')
       if (to.path !== undefined && !to.path.includes('search')) {
-        await this.$store.ep_set(this.search_ns, 'disable_get_data', true);
-        await this.$store.commit(this.search_ns + '/'+ 'RESET_ALL')
-        await this.$store.commit(this.search_ns + '/RESET_SEARCH_RESULTS')
+        await this.$store.ep_set(ns, 'disable_get_data', true);
+        await this.$store.commit(ns + '/'+ 'RESET_ALL')
+        await this.$store.commit(ns + '/RESET_SEARCH_RESULTS')
       }
       next()
     },
     async mounted() {
       this.$log.info('mounted')
-      await this.$store.ep_set(this.search_ns, 'disable_get_data', false);
+      let ns = VUEX_NAMESPACE
+      await this.$store.ep_set(ns, 'disable_get_data', false);
   },
   }
 </script>
